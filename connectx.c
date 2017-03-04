4dca7670aa0968630adcc28fec77e3ef6c6915be
derrikf2 <fleminde@mail.gvsu.edu>
2017-03-03 19:22:22 -0500

    to debug check funcs on small board

--- connectx.c
+++ connectx.c
@@ -15,9 +15,7 @@
 /********************************************************************/
 
 
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
+
 #include "handler.h"
 #include "game.h"
 
